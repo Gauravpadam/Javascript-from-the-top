d5ce670c747047e0494c303704ccd19c0bc81749
Gauravpadam <1032201077@tcetmumbai.in>
2023-08-24 11:24:14 +0530

    Datatypes in JS

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // declares the current standard of JS ES6
+
+let name = "Krishnamurthy" // string
+let age = 18 // int / number (ranges upto 2**53)
+let isLoggedIn = false // bool
+
+/* Primitive datatypes */
+// number => 2 to the power of 53
+// bigint
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined => simply undefined yet
+// symbol => unique
+
+// object
+
+console.log(typeof "Krishnamurthy"); // string
+
+// Here's when things start to get interesting
+console.log(typeof null); // null is an object(?!)
+console.log(typeof undefined); // undefined is undefined
